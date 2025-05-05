0ce8cbdf7eff17e9a37841265fd3756838c77314
AakaAltair <162553165+AakaAltair@users.noreply.github.com>
2025-05-05 19:55:04 +0530

    Update firebase.ts

--- app/firebase.ts
+++ app/firebase.ts
@@ -5,13 +5,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth"; // ✅ Added for authentication
 
 const firebaseConfig = {
-  apiKey: "AIzaSyASVgFGzkr43pOfcX7QzHaaHvqPxa85K5s",
-  authDomain: "web-apps-d4bee.firebaseapp.com",
-  projectId: "web-apps-d4bee",
-  storageBucket: "web-apps-d4bee.firebasestorage.app",
-  messagingSenderId: "933274496202",
-  appId: "1:933274496202:web:4b1073c5c054bb7e4f5833",
-  measurementId: "G-T13X4HJJW3",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 // Prevent re-initializing the app
